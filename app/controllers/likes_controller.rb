e1c19203bb7b54f23791d3908505bac11ad4184c
Elena <elena.korotich@gmail.com>
2017-01-19 17:14:43 -0800

    fixing deployment

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,20 +1,18 @@
 class InvitesController < ApplicationController
   # before_action :authenticate_user!
 
-respond_to :js
+	def like
+	  @user = current_user
+	  @post = Post.find(params[:post_id])
+	  @user.like!(@post)
+	end
 
-def like
-  @user = current_user
-  @post = Post.find(params[:post_id])
-  @user.like!(@post)
-end
-
-def unheart
-  @user = current_user
-  @like = @user.likes.find_by_post_id(params[:post_id])
-  @post = Post.find(params[:post_id])
-  @like.destroy!
-end
+	def unheart
+	  @user = current_user
+	  @like = @user.likes.find_by_post_id(params[:post_id])
+	  @post = Post.find(params[:post_id])
+	  @like.destroy!
+	end
 
 
 end
